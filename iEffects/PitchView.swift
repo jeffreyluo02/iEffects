b98770b386fe69a10cfb03d90573dd60442d71e4
jeffreyluo02 <jl834@duke.edu>
2019-11-16 18:15:11 -0500

    Finished Pitch Effect View

--- iEffects/PitchView.swift
+++ iEffects/PitchView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct PitchView: View {
     
     @ObservedObject var audioRecorder = AudioRecorder()
+    @ObservedObject var audioPlayer = AudioPlayer()
+    @State private var pitch : Float = 0
     @State private var recording = 0
     
     var body: some View {
@@ -23,11 +25,27 @@ struct PitchView: View {
                         }
                     }
                 }
-                Button("Play Recording") {
-                    // do something
+                Section(header: Text("Edit values")) {
+                    // edit the variables here
+                    VStack {
+                        Text("Pitch shift amount")
+                            .font(.headline)
+                        Slider(value: $pitch, in: -2400...2400, step: 100)
+                        Text("Your pitch shift is \(pitch, specifier: "%.f") cents")
+                    }
                 }
+                if(audioPlayer.isPlaying == false) {
+                    Button("Play Recording") {
+                        self.audioPlayer.playAudioWithVariablePitch(audio: self.audioRecorder.recordings[self.recording].fileURL, pitch: self.pitch)
+                    }
+                }
+                else {
+                        Button("Stop Recording") {
+                            self.audioPlayer.stopPlayback()
+                        }
+                    }
             }
-        .navigationBarTitle("Pitch Effect")
+            .navigationBarTitle("Pitch Effect")
         }
     }
 }
